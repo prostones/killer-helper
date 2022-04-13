7f0fe1c431cc8d80eea65546a403b94db4746b52
shaojundao <372248040@qq.com>
2022-04-14 00:13:29 +0800

    通过板子创建

--- web/src/api/api.ts
+++ web/src/api/api.ts
@@ -4,14 +4,22 @@ enum Api {
   CreateRoom = '/api/admin/create/',
   BackRoom = '/api/admin/coming/',
   ComingRoom = '/api/player/coming/',
+  QueryGameBook = '/api/game/book/list',
+  CreateByGameBookId = '/api/admin/create/by/',
 }
 
 export const createRoom = (total: string) => {
   return request.post<any>(Api.CreateRoom + total);
+};
+export const createByGameBookId = (gameBookId: string) => {
+  return request.post<any>(Api.CreateByGameBookId + gameBookId);
 };
 export const backRoom = (code: string) => {
   return request.post<any>(Api.BackRoom + code);
 };
 export const comingRoom = (code: string, number: string) => {
   return request.post<any>(Api.ComingRoom + code + '/' + number);
+};
+export const queryGameBook = () => {
+  return request.get<any>(Api.QueryGameBook);
 };
