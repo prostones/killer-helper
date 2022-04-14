d2cf65bbc5dd02d5a936288628b157827bb51f55
shilei.wang <shilei.wang@sumscope.com>
2022-04-14 12:16:44 +0800

    增加后端伪域名

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     host: "0.0.0.0",
     proxy: {
       '/api': {
-          target: 'http://killer-helper:8080',
+          target: 'http://localhost:8080',
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, '')
       }
