606befbf8726eaf33e4dd1dfa66e1d87fe81daec
shilei.wang <shilei.wang@sumscope.com>
2022-05-06 18:24:36 +0800

    xxx

--- src/main/java/com/nbteam/killer/helper/controller/AdminController.java
+++ src/main/java/com/nbteam/killer/helper/controller/AdminController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,12 +16,24 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class AdminController {
 
+    @Value("${app.version}")
+    private String version;
+
+    @Value("${app.build.time}")
+    private String buildTime;
+
     @Autowired
-    GameService gameService;
+    private GameService gameService;
 
     @GetMapping("index")
     public Result<?> index() {
         return Result.ok();
+    }
+
+    @GetMapping("/version")
+    @ApiOperation("获取版本号")
+    public Result<?> version() {
+        return Result.ok(version);
     }
 
     @PostMapping("/create/{total}")
