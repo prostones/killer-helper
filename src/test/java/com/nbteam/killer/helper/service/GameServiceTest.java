bb449a6ce470eef6aeb0923c01736fedd21af97e
shilei.wang <shilei.wang@sumscope.com>
2022-05-09 13:36:13 +0800

    调整单元测试

--- src/test/java/com/nbteam/killer/helper/service/GameServiceTest.java
+++ src/test/java/com/nbteam/killer/helper/service/GameServiceTest.java
@@ -1,5 +1,6 @@
 package com.nbteam.killer.helper.service;
 
+import cn.hutool.core.util.NumberUtil;
 import com.nbteam.killer.helper.KillerHelperApplication;
 import com.nbteam.killer.helper.enums.RoleEnum;
 import com.nbteam.killer.helper.repository.GameBookRepository;
@@ -30,14 +31,21 @@ class GameServiceTest {
         log.info("注意：使用板子" + book.getTitle() + "进行测试");
 
         int count = 0;
+        int langRenCount = 0;
         int total = 1000;
 
         for (int i = 0; i < 1000; i++) {
             if (gameService.create(book.getId()).getPlayers().stream().anyMatch(p -> p.getNumber().equals(1) && p.getRole().equals(RoleEnum.YU_YAN_JIA))) {
                 count++;
             }
+
+            if (gameService.create(book.getId()).getPlayers().stream().anyMatch(p -> p.getNumber().equals(1) && p.getRole().equals(RoleEnum.LANG_REN))) {
+                langRenCount++;
+            }
         }
 
-        log.info("1号预言家概率=>" + (count / total));
+        log.info("1号预言家概率=>" + NumberUtil.div(count, total));
+        log.info("1号狼概率=>" + NumberUtil.div(langRenCount, total));
+
     }
 }
