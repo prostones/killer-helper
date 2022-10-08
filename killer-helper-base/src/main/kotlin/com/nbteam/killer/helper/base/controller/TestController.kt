2d8b6bf5d173b30dd40fa5e0481d2d86d7dfee16
prostones <450113643@qq.com>
2022-10-08 23:30:50 +0800

    init kotlin jar

--- killer-helper-base/src/main/kotlin/com/nbteam/killer/helper/base/controller/TestController.kt
+++ killer-helper-base/src/main/kotlin/com/nbteam/killer/helper/base/controller/TestController.kt
@@ -1,4 +1,14 @@
 package com.nbteam.killer.helper.base.controller
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class TestController {
+    @GetMapping
+    fun index(): List<String> = listOf(
+        "Hello!",
+        "Bonjour!",
+        "Privet!",
+    )
 }
