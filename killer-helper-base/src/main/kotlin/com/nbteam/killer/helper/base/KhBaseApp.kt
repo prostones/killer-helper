a0e67ac695b04933f1a405c7d1386af2e0ad99c5
shilei.wang <shilei.wang@sumscope.com>
2022-10-09 13:06:50 +0800

    add demo

--- killer-helper-base/src/main/kotlin/com/nbteam/killer/helper/base/KhBaseApp.kt
+++ killer-helper-base/src/main/kotlin/com/nbteam/killer/helper/base/KhBaseApp.kt
@@ -1,11 +1,50 @@
 package com.nbteam.killer.helper.base
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.concurrent.Executors
+import kotlin.concurrent.thread
+import kotlin.system.measureTimeMillis
 
 @SpringBootApplication
 class KhBaseApp
 
 fun main(args: Array<String>) {
     runApplication<KhBaseApp>(*args)
+
+    trackTime { executorTest() } // 1264
+    //trackTime { ktLaunchTest() } // 926
+    //trackTime { threadTest() } // 6570
 }
+
+
+fun executorTest() = Executors.newCachedThreadPool().apply {
+    repeat(10000) {
+        execute {
+            println("${Thread.currentThread().name}")
+            //Thread.sleep(666)
+        }
+    }
+}
+
+fun ktLaunchTest() = runBlocking {
+    repeat(10000) {
+        launch {
+            println("${Thread.currentThread().name}")
+            //delay(666)
+        }
+    }
+}
+
+fun threadTest() = repeat(10000) {
+    thread {
+        println("${Thread.currentThread().name}")
+        //Thread.sleep(666)
+    }
+}
+
+fun trackTime(method: () -> Unit) = println("耗时->"+measureTimeMillis(method))
+
--- killer-helper-base/src/main/kotlin/com/nbteam/killer/helper/base/controller/TestController.kt
+++ killer-helper-base/src/main/kotlin/com/nbteam/killer/helper/base/controller/TestController.kt
@@ -1,14 +1,41 @@
 package com.nbteam.killer.helper.base.controller
 
+import cn.hutool.core.util.RandomUtil
+import com.nbteam.killer.helper.base.trackTime
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import lombok.extern.slf4j.Slf4j
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@Slf4j
 class TestController {
-    @GetMapping
+
+    @GetMapping("index")
     fun index(): List<String> = listOf(
         "Hello!",
         "Bonjour!",
         "Privet!",
     )
+
+    @GetMapping("loop")
+    fun loop(): String {
+
+        trackTime {
+            runBlocking {
+                repeat(100000) {
+                    GlobalScope.launch {
+                        println("${Thread.currentThread().name}-run"+RandomUtil.getRandom().nextLong())
+                        // Thread.sleep(2000)
+                        // println(i+"wake")
+                    }
+                }
+            }
+        }
+        return "loop"
+    }
+
 }
+
