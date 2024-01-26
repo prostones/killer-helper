c390dace30987dc7313d5f05b56bf440a2a1fade
shilei.wang <shilei.wang@sumscope.com>
2024-01-26 10:03:29 +0800

    优化打包脚本

--- build.bat
+++ build.bat
@@ -1,2 +1,8 @@
-docker build -t pro4stones/killer-helper .
-docker push pro4stones/killer-helper
+SET datetime=%date:~0,4%%date:~5,2%%date:~8,2%-%time:~0,2%%time:~3,2%%time:~6,2%
+SET datetime=%datetime: =0%
+
+docker build -t pro4stones/killer-helper:%datetime% .
+docker push pro4stones/killer-helper:%datetime%
+
+docker tag pro4stones/killer-helper:%datetime% pro4stones/killer-helper:latest
+docker push pro4stones/killer-helper:latest
