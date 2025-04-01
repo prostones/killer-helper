fe7230c11717c9d9a557e5a56a685481b32ea51d
shilei.wang <shilei.wang@sumscope.com>
2025-04-01 13:53:51 +0800

    add

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
+docker login -u pro4stones -p dckr_pat_dePZpZqEKe2yzO9J_ZACsFStCsg
 SET datetime=%date:~0,4%%date:~5,2%%date:~8,2%-%time:~0,2%%time:~3,2%%time:~6,2%
 SET datetime=%datetime: =0%
 
--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+
+docker login -u pro4stones -p dckr_pat_dePZpZqEKe2yzO9J_ZACsFStCsg
 datetime=$(date +"%Y%m%d-%H%M%S")
 docker build -t pro4stones/killer-helper:$datetime .
 docker push pro4stones/killer-helper:$datetime
